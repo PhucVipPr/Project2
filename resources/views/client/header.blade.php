a7013807ae432435d405f29838e145482f6e5ab4
hitwasabi <anhnamco9@gmail.com>
2023-01-31 15:42:09 +0700

    update

--- resources/views/client/header.blade.php
+++ resources/views/client/header.blade.php
@@ -48,42 +48,47 @@
     <div class="others">
         <form action="{{url('client/home/search')}}" method="get">
 {{--            {{csrf_field()}}--}}
-            <li><input name="keyword_submit" placeholder="Tìm kiếm" type="text"><i class="fa fa-search" style="padding-left: 10px"></i></li>
+            <li><input name="keyword_submit" placeholder="Tìm kiếm" type="text"> <i class="fa fa-search"></i></li>
         </form>
-        <li> <div class="dropdown">
-                @if(Auth::id()==0)
-                <button class="fa fa-user" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                User
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <form action="{{url('login')}}" method="POST">
-                        @csrf
-                        &nbsp;&nbsp;<button class="fa fa-sign-in"> Login</button>
-                    </form>
-                </div>
-                @else
-                    <button class="fa fa-user" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{Auth::user()->name}}
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <form action="{{url('client/info')}}" method="get">
-                            @csrf
-                            &nbsp;&nbsp;<button class="fa fa-info-circle"> Info</button>
-                        </form>
-                        <form action="{{url('client/clientOrder')}}" method="GET">
-                            @csrf
-                            &nbsp;
-                              <button class="fa fa-bookmark"> Order</button>
-                        </form>
-                        <form action="{{url('logout')}}" method="POST">
-                            @csrf
-                            &nbsp;&nbsp;<button class="fa fa-sign-out"> Logout</button>
-                        </form>
+                @if(Auth::check() == false)
+                    <div class="dangnhap-dangki">
+                        <a href="/login" class="dang-nhap"> Đăng nhập /</a>
+                        <a href="/register" class="dang-ki"> Đăng ký </a>
+                        @else
+                            <li class="dangnhap-dangki">
+                                @if(Auth::id()==0)
+                                    <button class="fa fa-user" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <form action="{{url('login')}}" method="POST">
+                                            @csrf
+                                            &nbsp;&nbsp;<button class="fa fa-sign-in"> Login</button>
+                                        </form>
+                                    </div>
+                                @else
+                                    <button class="fa fa-user" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        {{Auth::user()->name}}
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <form action="{{url('client/info')}}" method="get">
+                                            @csrf
+                                            &nbsp;&nbsp;<button class="fa fa-info-circle"> Info</button>
+                                        </form>
+                                        <form action="{{url('client/clientOrder')}}" method="GET">
+                                            @csrf
+                                            &nbsp;
+                                            <button class="fa fa-bookmark"> Order</button>
+                                        </form>
+                                        <form action="{{url('logout')}}" method="POST">
+                                            @csrf
+                                            &nbsp;&nbsp;<button class="fa fa-sign-out"> Logout</button>
+                                        </form>
+                                    </div>
+                                   <li> <a class="fa fa-shopping-cart" href="{{url('client/cartList')}}"></a></li>
+                                @endif
+                            </div>
                     </div>
-                @endif
-            </div>
-        </li>
-        <li> <a class="fa fa-shopping-cart" href="{{url('client/cartList')}}"></a></li>
+            @endif
     </div>
 </header>
 @include('sweetalert::alert')
