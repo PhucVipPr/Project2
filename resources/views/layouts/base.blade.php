f030139eddd730cc8ed20c0bcae776543c67ff7a
hitwasabi <anhnamco9@gmail.com>
2023-05-25 22:56:24 +0700

    chat onl

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -46,6 +46,19 @@
 
 </head>
 <!-- body -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/646f826ead80445890ef1b09/1h19pb0c2';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <body class="main-layout">
 <!-- loader  -->
 <!-- end loader -->
